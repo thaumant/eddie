72910562a7b9a4cc02eb637fd421f0cfc27a0842
Elia Schelokov <thaumant@gmail.com>
2019-11-09 13:09:17 +0300

    Faster iterations and distance matrix representation

--- src/lev.rs
+++ src/lev.rs
@@ -1,47 +1,88 @@
 use std::cmp;
 
-const MAX_SIZE: usize = 16;
 
 
+const MAX_CHARS: usize = 15;
+
+
+#[derive(Debug)]
 pub struct Lev {
-    dists: [[u8; MAX_SIZE + 1]; MAX_SIZE + 1],
+    len1: usize,
+    len2: usize,
+    chars1: [char; MAX_CHARS],
+    chars2: [char; MAX_CHARS],
+    dists: [[u8; MAX_CHARS + 1]; MAX_CHARS + 1],
 }
 
 
 impl Lev {
     pub fn new() -> Lev {
-        let dists = [[0; MAX_SIZE + 1]; MAX_SIZE + 1];
-        Lev { dists }
+        let len1 = 0;
+        let len2 = 0;
+        let chars1 = ['0'; MAX_CHARS];
+        let chars2 = ['0'; MAX_CHARS];
+        let dists = [[0; MAX_CHARS + 1]; MAX_CHARS + 1];
+        Lev { len1, len2, chars1, chars2, dists }
     }
 
-    pub fn dist<'a>(&mut self, str1: &'a str, str2: &'a str) -> u8 {
-        let i1_max = cmp::min(str1.len(), MAX_SIZE);
-        let i2_max = cmp::min(str2.len(), MAX_SIZE);
+    pub fn dist(&mut self, str1: &str, str2: &str) -> u8 {
+        self.len1 = cmp::min(str1.len(), MAX_CHARS);
+        self.len2 = cmp::min(str2.len(), MAX_CHARS);
 
-        for i1 in 0..i1_max + 1 { self.dists[i1][0] = i1 as u8; }
-        for i2 in 0..i2_max + 1 { self.dists[0][i2] = i2 as u8; }
+        for (i, c) in str1.chars().take(self.len1).enumerate() { self.chars1[i] = c; }
+        for (i, c) in str2.chars().take(self.len2).enumerate() { self.chars2[i] = c; }
 
-        for (_i1, char1) in str1.chars().take(i1_max).enumerate() {
-            for (_i2, char2) in str2.chars().take(i2_max).enumerate() {
-                let i1 = _i1 + 1;
-                let i2 = _i2 + 1;
+        for i in 0..self.len1 + 1 { self.dists[i][0] = i as u8; }
+        for i in 0..self.len2 + 1 { self.dists[0][i] = i as u8; }
+
+        for row in 1..self.len1 + 1 {
+            for col in 1..self.len2 + 1 {
+                let char1 = self.chars1[row - 1];
+                let char2 = self.chars2[col - 1];
 
                 let cost_sub = if char1 == char2 { 0 } else { 1 };
 
-                let dist_del = self.dists[i1 - 1][i2] + 1;
-                let dist_add = self.dists[i1][i2 - 1] + 1;
-                let dist_sub = self.dists[i1 - 1][i2 - 1] + cost_sub;
+                let dist_del = self.dists[row - 1][col] + 1;
+                let dist_add = self.dists[row][col - 1] + 1;
+                let dist_sub = self.dists[row - 1][col - 1] + cost_sub;
 
-                let mut dist_min = 3;
+                let mut dist_min = cmp::max(self.len1, self.len2) as u8;
                 dist_min = cmp::min(dist_min, dist_del);
                 dist_min = cmp::min(dist_min, dist_add);
                 dist_min = cmp::min(dist_min, dist_sub);
 
-                self.dists[i1][i2] = dist_min;
+                self.dists[row][col] = dist_min;
             }
         }
 
-        self.dists[i1_max][i2_max]
+        self.dists[self.len1][self.len2]
+    }
+
+    pub fn repr(&self) -> String {
+        let Lev {len1, len2, ..} = *self;
+        let mut repr = String::with_capacity((len1 + 2) * (len2 + 2));
+
+        repr.push_str(&format!("{} x {}\n", len1, len2));
+
+        repr.push_str(&"    ");
+        for col in 0..len1 {
+            repr.push(self.chars1[col]);
+            repr.push(' ');
+        }
+        repr.push('\n');
+
+        for row in 0..len2 + 1 {
+            repr.push(if row == 0 { ' ' } else { self.chars2[row - 1] });
+            repr.push(' ');
+            for col in 0..len1 + 1 {
+                repr.push_str(&self.dists[col][row].to_string());
+                repr.push(' ');
+            }
+            repr.push('\n');
+        }
+        repr.push('\n');
+
+        repr
     }
 }
 
@@ -58,22 +99,36 @@ mod tests {
         assert_eq!(lev.dist("c", "c"), 0);
         assert_eq!(lev.dist("ca", "ca"), 0);
         assert_eq!(lev.dist("cap", "cap"), 0);
+        assert_eq!(lev.dist("capt", "capt"), 0);
+        assert_eq!(lev.dist("capta", "capta"), 0);
+        assert_eq!(lev.dist("captai", "captai"), 0);
+        assert_eq!(lev.dist("captain", "captain"), 0);
     }
 
     #[test]
-    fn left_empty() {
+    fn left_prefix() {
         let mut lev = Lev::new();
-        assert_eq!(lev.dist("", "c"), 1);
-        assert_eq!(lev.dist("", "ca"), 2);
-        assert_eq!(lev.dist("", "cap"), 3);
+        assert_eq!(lev.dist("", "captain"), 7);
+        assert_eq!(lev.dist("c", "captain"), 6);
+        assert_eq!(lev.dist("ca", "captain"), 5);
+        assert_eq!(lev.dist("cap", "captain"), 4);
+        assert_eq!(lev.dist("capt", "captain"), 3);
+        assert_eq!(lev.dist("capta", "captain"), 2);
+        assert_eq!(lev.dist("captai", "captain"), 1);
+        assert_eq!(lev.dist("captain", "captain"), 0);
     }
 
     #[test]
-    fn right_empty() {
+    fn right_prefix() {
         let mut lev = Lev::new();
-        assert_eq!(lev.dist("c", ""), 1);
-        assert_eq!(lev.dist("ca", ""), 2);
-        assert_eq!(lev.dist("cap", ""), 3);
+        assert_eq!(lev.dist("captain", ""), 7);
+        assert_eq!(lev.dist("captain", "c"), 6);
+        assert_eq!(lev.dist("captain", "ca"), 5);
+        assert_eq!(lev.dist("captain", "cap"), 4);
+        assert_eq!(lev.dist("captain", "capt"), 3);
+        assert_eq!(lev.dist("captain", "capta"), 2);
+        assert_eq!(lev.dist("captain", "captai"), 1);
+        assert_eq!(lev.dist("captain", "captain"), 0);
     }
 
     #[test]
